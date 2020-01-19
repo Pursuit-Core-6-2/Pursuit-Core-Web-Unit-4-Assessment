a6920720e6890aaac32e29460649a96d04df1270
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-19 11:43:18 -0500

    0.2, add browserrouter routing init into site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 
 import './reset.css';
 import './App.css';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-
+      <Route path={'/'} component={Home} />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import axios from 'axios';
+
+// import './Home.css';
+
+export default class Home extends Component {
+  state = {
+
+  }
+
+
+  render() {
+    return(
+      <div>
+        HIT
+      </div>
+    );
+  }
+}
