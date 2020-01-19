fa65252055d417609628bbfdf7182208e54ec1a3
tbriany <brianytaveras@pursuit.org>
2020-01-19 17:22:28 -0500

    added description to about page

--- youtube-app/src/Components/About.jsx
+++ youtube-app/src/Components/About.jsx
@@ -5,7 +5,10 @@ class About extends Component {
     return (
         <div>
             <h1>About</h1>
-            <p>About this website...</p>
+            <p>This is a web app that allows users to search for videos using the YouTube Api. 
+                A search for a video returns the 8 most popular results. From those results users can 
+                click on a video to watch it, if they do click to watch it user's are redirected to a page where
+                they can watch the video and leave comments with their names. Created by Briany Taveras.</p>
         </div>
     )
  }
