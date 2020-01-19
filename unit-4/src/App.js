d73ac4c9b6dbae7ffbaedadd7d251769545750b1
kadjiah <kadijahwilson@pursuit.org>
2020-01-19 12:34:21 -0500

    able to fetch the urls from the youtube API, handle submit prints video array in the console for right now

--- unit-4/src/App.js
+++ unit-4/src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About';
+import Videos from './components/Videos'
 
 function App() {
   return (
@@ -11,8 +12,7 @@ function App() {
       <NavBar />
       <Switch>
         <Route exact path ="/" component={Home} /> 
-        {/* <Route path="/search" component={search} />
-        <Route path="/videos" component={videos} /> */}
+        <Route path="/videos/:id" component={Videos} />
         <Route path="/about" component={About} />
        </Switch>
     </div>
--- unit-4/src/components/Home.jsx
+++ unit-4/src/components/Home.jsx
@@ -1,17 +1,20 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import Axios from 'axios'
+import { Link } from 'react-router-dom';
+import API_KEY from '../secrets'
 
 
 class Home extends Component {
-    constructor(){
+    constructor() {
         super()
-        this.state ={
-            searchVids: '', 
-            resArr: [],
+        this.state = {
+            searchVids: '',
+            videos: []
             // results: " ",
         }
     }
-    
-    handleChange =(e) => {
+
+    handleChange = (e) => {
         this.setState({
             searchVids: e.target.value
         })
@@ -19,23 +22,33 @@ class Home extends Component {
 
     handleSubmit = async (e) => {
         e.preventDefault()
-        const {searchVids} = this.state
+        const { searchVids } = this.state
         console.log("searching for:", searchVids)
+        try {
+            const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=${API_KEY}&search=${searchVids}`
+            let res = await Axios.get(url)
+            console.log(res.data.items)
+            this.setState({
+                videos: res.data.items
+            })
+        } catch (err) {
+            console.log("error:", err)
+        }
     }
 
-    
 
-render() {
-    const {searchVids, resArr} = this.state
-    return (
-        <div>
-<form onSubmit={this.handleSubmit}>
-    <input type="text" placeholder="Search" onChange={this.handleChange} value={searchVids} />
-    <input type="submit"/>
-</form>
-        </div>
+
+    render() {
+        const { searchVids, videos } = this.state
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Search" onChange={this.handleChange} value={searchVids} />
+                    <input type="submit" />
+                </form>
+          </div>
     )
-}
+    }
 }
 
 export default Home;
