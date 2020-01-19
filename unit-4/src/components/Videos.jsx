d19ffcf58461d59637fe0aae23be96678847ea95
kadjiah <kadijahwilson@pursuit.org>
2020-01-19 13:50:00 -0500

    creating Video class to hold the likes, comments and vid url

--- unit-4/src/components/Videos.jsx
+++ unit-4/src/components/Videos.jsx
@@ -3,3 +3,24 @@ import axios from 'axios'
 import API_KEY from '../secrets'
 
 
+class Videos extends Component {
+    constructor(){
+        super()
+            this.state({
+                likes: 0,
+                vids: " ",
+                comments: ""
+            })
+    }
+
+
+
+    render() {
+        
+        return(
+<div>
+
+</div>
+        )
+    }
+}
