73b6ad4469b03dd9864599754768e0284241fbce
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-19 17:45:46 -0500

    0.8, aboutpage written

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -5,25 +5,44 @@ Aboutpage Component | YouTube Abbreviated | Unit 4 Assessment
 
 
 /* IMPORTS */
-import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import axios from 'axios';
+import React from 'react';
 
-// import './About.css';
+// import './Aboutpage.css';
 
 
 /* COMPONENT + EXPORT */
-export default class Aboutpage extends Component {
-  state = {
+const Aboutpage = () => {
 
-  }
+  return(
+    <div className="stage">
+      <h1>About the Developer</h1>
+      <div className="body-about">
+        
+        <p>
+          YouTube Abbreviated takes the user's inputed search and returns eight
+          of the top results from the YouTube API (Thanks, 
+            YouTube). 
+          The user is then able to watch any of the results in the video 
+          result's own page which allows comments to be submitted and saved 
+          (temporarily for now).
+        </p>
+          
+        <p>
+          YouTube Abbreviated is at it's core a kick-a** React app developed
+          under the shadow of the Unit 4 Assessment Component of the famed
+          Pursuit 
+          real-life app.
+        </p>
+        
+        <p>
+          Regards,<br />
+          <strong>Joseph P. Pasaoa, developer</strong>
+        </p>
 
-
-  render() {
-    return(
-      <div>
-        About
       </div>
-    );
-  }
+
+    </div>
+  );
 }
+
+export default Aboutpage;
