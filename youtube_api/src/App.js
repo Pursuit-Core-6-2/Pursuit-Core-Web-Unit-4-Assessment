b2080e4bbce72839f8a4916d1686892121356dcc
jonathan <jonathanfagan@pursuit.org>
2020-01-19 15:27:17 -0500

    base work done

---
+++ youtube_api/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link, Switch, Route } from 'react-router-dom'
+import HomePage from "./components/homepage"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav id="header">
+        <p>YouTube</p>{" "}
+        <Link to="/">Home</Link>{" "}
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Switch>
+        <Route exact path = "/" component = {HomePage}/>
+      </Switch>
+      
+
+
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
---
+++ youtube_api/src/components/homepage.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react"
+import secrets from "./secrets"
+import axios from "axios"
+import YouTube from 'react-youtube'
+import YouTubeVid from './youtubeVid'
+
+class HomePage extends Component {
+    constructor() {
+        super()
+        this.state = {
+            key: secrets.API_KEY,
+            search_term: "",
+            videos: []
+        }
+    }
+
+
+    handleChange = (e) => {
+        console.log("Search Term:", e.target.value)
+        this.setState({
+            search_term: e.target.value
+        })
+
+    }
+    
+    onReady(event) {
+        // access to player in all event handlers via event.target
+        event.target.pauseVideo();
+      }
+    
+
+    handleFromSubmit = (e) => {
+        let { key, search_term } = this.state
+        e.preventDefault()
+
+        try {
+
+            let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${search_term}&key=${key}`
+            axios.get(url)
+                .then(res => {
+                    console.log(res.data.items[0].snippet.thumbnails.default.url)
+                    this.setState({
+                        videos: res.data.items
+                    })
+
+
+
+                })
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    
+
+    render() {
+        let { search_term, videos } = this.state
+        const opts = {
+            height: '390',
+            width: '640',
+            playerVars: {  
+            //   origin:  "https://localhost:3000",
+              autoplay: 0,
+              enablejsapi: 1,
+              src: "http://localhost:3000"
+            }
+          };
+          
+        
+        
+        console.log(YouTube)
+        return (
+            <div id="search-field">
+                <form onSubmit={this.handleFromSubmit}>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={search_term}
+                        onChange={this.handleChange}
+                    />
+                    <button>Search</button>
+                </form>
+
+                <div>
+                    {videos.map((el, index) => {
+                        return <YouTube 
+                        onReady = {this.onReady}
+                        key = {index} 
+                        videoId = {el.id.channelId} 
+                        opts = {opts} 
+                       />
+                        // <YouTubeVid 
+                        // title = {el.snippet.title}
+                        // url = {el.snippet.thumbnails.default.url}
+                        // desc = {el.snippet.desc}
+                        // key = {index}/>
+
+                    })}
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default HomePage
