5c86bb31f9454a89650d301b393fdd579fc107e1
joseph-p-pasaoa <joseph.p.pasaoa@gmail.com>
2020-01-21 19:32:03 -0500

    1.6.1, style navbar now playing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -64,7 +64,7 @@ export default class NavBar extends Component {
             return (
               <li className="now-playing">
                 <div className="now-playing-label">
-                  Now Playing:
+                  Now Playing
                 </div>
                 {nowPlayingTitle}
               </li>
