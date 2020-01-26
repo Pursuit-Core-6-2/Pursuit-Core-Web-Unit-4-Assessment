949a3dae5c14f456cea4e51f29fd78e660dbbc5c
KathyPuma <kathypuma96@gmail.com>
2020-01-26 15:14:23 -0500

    included an if statement to show a message if there are no results

--- client-hooks/src/Home.js
+++ client-hooks/src/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import API_KEY from './secrets.js'
 
@@ -8,47 +8,47 @@ const Home = () => {
     const [search, setSearch] = useState('')
     const [searchResults, setSearchResults] = useState([])
 
-
-    useEffect (() =>{
-        console.log("okay")
+    useEffect(() => {
         getThumbnails()
     }, [])
-
 
     const getThumbnails = async () => {
         try {
             const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=8&q=${search}&key=${API_KEY}&type=video`
             let response = await axios.get(url)
-            console.log(response.data.items)
             setSearchResults(response.data.items)
+            setSearch('')
 
         } catch (error) {
             console.log(error)
         }
     }
 
-
-
-
     return (
         <div className="Home">
             <input className="searchInput" type="text" placeholder="Search video" value={search} onChange={(e) => setSearch(e.target.value)} />
             <button className="submitButton" onClick={getThumbnails}> Submit </button>
 
-            <div className="videoListing">{
-                searchResults.map(result => {
-                    return (
-                        <Link to={`/video/${result.id.videoId}`}>
-                            <img className="searchResults" src={result.snippet.thumbnails.high.url} />
-                            <h3 className="searchTitleName">{result.snippet.title}</h3>
-                        </Link>
-                    )
-                })
-            }
-            </div>
+            {!searchResults.length > 0 ? (
+                <div>
+                    <p className="noResult">No Search Results Yet! Please Submit a Search Above!</p>
+                </div>
 
+            ) : (
+                    <div className="videoListing">{
+                        searchResults.map(result => {
+                            return (
+                                <Link to={`/video/${result.id.videoId}`}>
+                                    <img className="searchResults" src={result.snippet.thumbnails.high.url} />
+                                    <h3 className="searchTitleName">{result.snippet.title}</h3>
+                                </Link>
+                            )
+                        })
+                    }
+                    </div>
+                )
+            }
         </div>
     );
 }
-
 export default Home;
