b42f391d26df8a176e6ce36bbd4c62ec2772bf82
MikePursuit <michaelamparo@pursuit.org>
2020-01-28 14:07:03 -0500

    converted App.js to functional component

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,56 +1,38 @@
-import React, { Component } from 'react';
-import NavBar from './Components/NavBar'
-import YouTube from 'react-youtube'
-import { Route, Switch } from 'react-router-dom'
-import {Container} from 'react-bootstrap'
-
-
+import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Comments from './Components/Comments';
+import NavBar from './Components/NavBar';
+import Search from './Components/Search';
+import About from './Components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Search from './Components/Search';
-import Comments from './Components/Comments';
-import About from './Components/About';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            comments: {}
-        }
-        console.log("App constructor called")
-    }
-    componentDidMount = () => console.log("componentDidMount")
-    componentDidUpdate = () => console.log("componentDidUpdate")
+const App = () => {
+    const [comment, setComment] = useState({})
 
-    setComments = (id, comments) => {
-        this.setState({
-            [id]: comments
-        })
+    const setComments = (id, comments) => {
+        let obj = {...comment}
+        obj[id] = comments
+        setComment(obj)
     }
 
 
-    render() {
-        console.log("render called")
-
-
-        return (
-            <div className="App">
-                <NavBar />
-                <Container>
-                    {/*  */}
-                    <br />
-                    <Switch>
-                        <Route path='/about' render={() => <About />}/>
-                        <Route path='/video/:id' 
-                            render={(props) => <Comments comments={this.state[props.match.params.id]} setComments={this.setComments} videoID={props.match.params.id} />}
-                        />
-                        <Route path='/' render={() => <Search />}/>
-                    </Switch>
-                </Container>
-            </div>
-        );
-    }
-
+    return (
+        <div className="App">
+            <NavBar />
+            <Container>
+                <br />
+                <Switch>
+                    <Route path='/about' render={() => <About />}/>
+                    <Route path='/video/:id' 
+                        render={(props) => <Comments comments={comment[props.match.params.id]} setComments={setComments} videoID={props.match.params.id} />}
+                    />
+                    <Route path='/' render={() => <Search />}/>
+                </Switch>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
