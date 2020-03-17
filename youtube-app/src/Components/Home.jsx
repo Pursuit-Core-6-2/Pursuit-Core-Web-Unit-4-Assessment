d188f739ef426b4e0682fa622a77592ecce71a7e
Jenesh <jenesh.napit@gmail.com>
2020-03-17 19:52:52 -0400

    Added some tests to Home component

--- youtube-app/src/Components/Home.jsx
+++ youtube-app/src/Components/Home.jsx
@@ -1,43 +1,50 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-import { Input, Button } from 'semantic-ui-react'
-import { getVideoDetails } from '../helperFunctions/apiCalls'
+import React from "react";
+import { withRouter, useHistory } from "react-router-dom";
+import { Input, Button } from "semantic-ui-react";
+import { getVideoDetails } from "../helperFunctions/apiCalls";
 
-const Home = (props) => {
-    const openVideo = async (id) => {
-        const data = await getVideoDetails(id)
-        props.changeVidDetails(data.data)
-        props.history.push(`/videos/${id}`)
-    }
-    
-    return (
-        <div>
-            <Input
-                action={<Button onClick={(e) => props.enterSearch(e)}>Search</Button>}
-                placeholder='Search...'
-                onChange={(e) => props.changeSearch(e)}
-                onKeyUp={(e) => props.enterSearch(e)}
-                className='SearchBar'
-            />
-            <div className='VideosContainer'>
-                {props.videoList.map(vid => {
-                    return (
-                        <div key={vid.id.videoId} id={vid.id.videoId} className='VideoDetails'>
-                            <img
-                                src={vid.snippet.thumbnails.medium.url}
-                                onClick={() => openVideo(vid.id.videoId)}
-                                className='VideoThumbnail'
-                                alt="Thumbnail"
-                            />
-                            <h4 onClick={() => openVideo(vid.id.videoId)}>
-                                {vid.snippet.title}
-                            </h4>
-                        </div>
-                    )
-                })}
+const Home = props => {
+  const history = useHistory();
+  const openVideo = async id => {
+    const data = await getVideoDetails(id);
+    props.changeVidDetails(data.data);
+    history.push(`/videos/${id}`);
+  };
+
+  return (
+    <div>
+      <img src="https://firebasestorage.googleapis.com/v0/b/portfoliosite-f91b2.appspot.com/o/YoutubeApp.gif?alt=media&token=7e334c9a-def4-4099-bd7c-5ba35f735722" />
+      <Input
+        action={<Button onClick={e => props.enterSearch(e)}>Search</Button>}
+        placeholder="Search..."
+        onChange={e => props.changeSearch(e)}
+        onKeyUp={e => props.enterSearch(e)}
+        className="SearchBar"
+      />
+      <div className="VideosContainer">
+        {props.videoList.map(vid => {
+          return (
+            <div
+              key={vid.id.videoId}
+              id={vid.id.videoId}
+              className="VideoDetails"
+            >
+              <img
+                src={vid.snippet.thumbnails.medium.url}
+                onClick={() => openVideo(vid.id.videoId)}
+                className="VideoThumbnail"
+                alt="Thumbnail"
+              />
+              <h4 onClick={() => openVideo(vid.id.videoId)}>
+                {vid.snippet.title}
+              </h4>
             </div>
-        </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default withRouter(Home)
+// export default withRouter(Home)
+export default Home;
---
+++ youtube-app/src/mockTestData/videoList.js
@@ -0,0 +1,104 @@
+export const list = [
+  {
+    kind: "youtube#searchResult",
+    etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/gqr25GOpOevQ73ymg9cqkZGvHFU"',
+    id: {
+      kind: "youtube#video",
+      videoId: "l0U7SxXHkPY1",
+    },
+    snippet: {
+      publishedAt: "2020-01-10T05:00:08.000Z",
+      channelId: "UCFNosi99Sp0_eLilBiXmmXA",
+      title: "Future - Life Is Good (Official Music Video) ft. Drake",
+      description:
+        '"Life is Good" available at: https://smarturl.it/LifeIsGood.Future Official Audio: https://youtu.be/ZBOoVj6IW3s Directed by Director X Future online: ...',
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "FutureVEVO",
+      liveBroadcastContent: "none",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/gqr25GOpOevQ73ymg9cqkZGvHFU"',
+    id: {
+      kind: "youtube#video",
+      videoId: "l0U7SxXHkPY2",
+    },
+    snippet: {
+      publishedAt: "2020-01-10T05:00:08.000Z",
+      channelId: "UCFNosi99Sp0_eLilBiXmmXA",
+      title: "Future - Life Is Good (Official Music Video) ft. Drake",
+      description:
+        '"Life is Good" available at: https://smarturl.it/LifeIsGood.Future Official Audio: https://youtu.be/ZBOoVj6IW3s Directed by Director X Future online: ...',
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "FutureVEVO",
+      liveBroadcastContent: "none",
+    },
+  },
+  {
+    kind: "youtube#searchResult",
+    etag: '"SJZWTG6xR0eGuCOh2bX6w3s4F94/gqr25GOpOevQ73ymg9cqkZGvHFU"',
+    id: {
+      kind: "youtube#video",
+      videoId: "l0U7SxXHkPY3",
+    },
+    snippet: {
+      publishedAt: "2020-01-10T05:00:08.000Z",
+      channelId: "UCFNosi99Sp0_eLilBiXmmXA",
+      title: "Future - Life Is Good (Official Music Video) ft. Drake",
+      description:
+        '"Life is Good" available at: https://smarturl.it/LifeIsGood.Future Official Audio: https://youtu.be/ZBOoVj6IW3s Directed by Director X Future online: ...',
+      thumbnails: {
+        default: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/default.jpg",
+          width: 120,
+          height: 90,
+        },
+        medium: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/mqdefault.jpg",
+          width: 320,
+          height: 180,
+        },
+        high: {
+          url: "https://i.ytimg.com/vi/l0U7SxXHkPY/hqdefault.jpg",
+          width: 480,
+          height: 360,
+        },
+      },
+      channelTitle: "FutureVEVO",
+      liveBroadcastContent: "none",
+    },
+  },
+];
